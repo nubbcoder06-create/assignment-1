fe20f52a80403cde75b9163a42d0f147f656b9f3
nubbcoder06-create <nubbcoder06@gmail.com>
2025-08-24 21:38:28 +0530

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,4 @@
+a = input("enter first name:")
+b = input("enter last name:")
+
+print("Hello,", a + b,"! Welcome to Python program.")
---
+++ simple_program.py
@@ -0,0 +1,7 @@
+a = int(input("enter first number:"))
+b = int(input("enter second number:"))
+
+print("addition =",a+b)
+print("subtraction =",a-b)
+print("multiplication =",a*b)
+print("division =",a/b)
